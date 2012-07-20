59c9ee1dd6e0523ccbef55c69f62d51698d9a7ef
JMather <me@jacobmather.com>
2012-07-19 23:09:05 -0300

    Added a resize bounce deflector that can be used. See http://paulirish.com/2009/throttled-smartresize-jquery-event-handler/ for more information on the basic concept of the problem domain.

--- appendAround.js
+++ appendAround.js
@@ -6,22 +6,32 @@ how-to:
 	4. Call appendAround() on that element when the DOM is ready
 */
 (function( $ ){
-	$.fn.appendAround = function(){
+	$.fn.appendAround = function(options){
 	  return this.each(function(){
       
 	    var $self = $( this ),
 	        att = "data-set",
-	        $set = $( "["+ att +"='"+ $self.closest( "["+ att +"]" ).attr( att ) + "']" );
+	        $set = $( "["+ att +"='"+ $self.closest( "["+ att +"]" ).attr( att ) + "']" ),
+	        $timer = null;
       
 	    function appendToVisibleContainer(){
 	      if( $self.is( ":hidden" ) ){
 	        $self.appendTo( $set.filter( ":visible:eq(0)" ) );
 	      }
 	    }
+	    
+            // As resize is called constantly, it is likely a good idea to add a bit
+            // of a buffer. Paul Irish does a good job summarizing the issue:
+            // http://paulirish.com/2009/throttled-smartresize-jquery-event-handler/
+            function bufferResize() {
+                if ($timer != null)
+                    clearTimeout($timer);
+                $timer = setTimeout(appendToVisibleContainer, 100)
+            }
+
+            appendToVisibleContainer();
       
-	    appendToVisibleContainer();
-      
-	    $(window).resize( appendToVisibleContainer );
+	    $(window).resize( (options && options.buffer == true) ? bufferResize : appendToVisibleContainer );
       
 	  });
 	};
