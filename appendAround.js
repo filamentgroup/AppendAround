014301bd7ea489ff2746fbafe8ac3b80b93be61e
Scott Jehl <scott@scottjehl.com>
2013-02-14 14:50:54 -0600

    edits for more reliable cross-library intregration.

--- appendAround.js
+++ appendAround.js
@@ -11,17 +11,31 @@ how-to:
       
 	    var $self = $( this ),
 	        att = "data-set",
-	        $set = $( "["+ att +"='"+ $self.closest( "["+ att +"]" ).attr( att ) + "']" );
-      
-	    function appendToVisibleContainer(){
-	      if( $self.is( ":hidden" ) ){
-	        $self.appendTo( $set.filter( ":visible:eq(0)" ) );
-	      }
+	        $parent = $self.parent(), 
+	        parent = $parent[ 0 ],
+	        attval = $parent.attr( att ),
+	        $set = $( "["+ att +"='" + attval + "']" );
+
+		function isHidden( elem ){
+			return window.getComputedStyle( elem ,null).getPropertyValue( "display" ) === "none";
+		}
+
+		function appendToVisibleContainer(){
+			if( isHidden( parent ) ){
+				var found = 0;
+				$set.each(function(){
+					if( !isHidden( this ) && !found ){
+						$self.appendTo( this );
+						found++;
+						parent = this;
+					}
+				});
+	      	}
 	    }
       
 	    appendToVisibleContainer();
       
-	    $(window).resize( appendToVisibleContainer );
+	    $(window).bind( "resize", appendToVisibleContainer );
       
 	  });
 	};
