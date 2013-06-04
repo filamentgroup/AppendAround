2bdbe3442d89adea56293d116864bc9c31fe3ceb
Rob Miller <rob@bigfish.co.uk>
2013-06-04 16:53:02 +0100

    More concise logic
    Since this is already a jQuery plugin, we can use some built-in jQuery
    functionality to condense the logic down to just three lines.

--- appendAround.js
+++ appendAround.js
@@ -12,24 +12,13 @@ how-to:
 	    var $self = $( this ),
 	        att = "data-set",
 	        $parent = $self.parent(), 
-	        parent = $parent[ 0 ],
 	        attval = $parent.attr( att ),
 	        $set = $( "["+ att +"='" + attval + "']" );
 
-		function isHidden( elem ){
-			return window.getComputedStyle( elem ,null).getPropertyValue( "display" ) === "none";
-		}
-
 		function appendToVisibleContainer(){
-			if( isHidden( parent ) ){
-				var found = 0;
-				$set.each(function(){
-					if( !isHidden( this ) && !found ){
-						$self.appendTo( this );
-						found++;
-						parent = this;
-					}
-				});
+			if( !$parent.is(':visible') ){
+				$parent = $set.filter(':visible').first();
+				$self.appendTo($parent);
 	      	}
 	    }
       
