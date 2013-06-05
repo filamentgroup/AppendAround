466bf55fd250894a509cf1508b86d0748be41a92
Rob Miller <rob@bigfish.co.uk>
2013-06-05 11:00:09 +0100

    Consistent whitespace and code style

--- appendAround.js
+++ appendAround.js
@@ -1,31 +1,28 @@
-/*! appendAround markup pattern. [c]2012, @scottjehl, Filament Group, Inc. MIT/GPL 
-how-to:
-	1. Insert potential element containers throughout the DOM
-	2. give each container a data-set attribute with a value that matches all other containers' values
-	3. Place your appendAround content in one of the potential containers
-	4. Call appendAround() on that element when the DOM is ready
-*/
+/*! appendAround markup pattern. [c]2012, @scottjehl, Filament Group, Inc. MIT/GPL
+  how-to:
+  1. Insert potential element containers throughout the DOM
+  2. give each container a data-set attribute with a value that matches all other containers' values
+  3. Place your appendAround content in one of the potential containers
+  4. Call appendAround() on that element when the DOM is ready
+  */
 (function( $ ){
 	$.fn.appendAround = function(){
-	  return this.each(function(){
-      
-	    var $self = $( this ),
-	        att = "data-set",
-	        $parent = $self.parent(), 
-	        attval = $parent.attr( att ),
-	        $set = $( "["+ att +"='" + attval + "']" );
+		return this.each(function(){
+			var $self = $( this ),
+				$parent = $self.parent(),
+				att = "data-set",
+				attval = $parent.attr( att ),
+				$set = $( "["+ att +"='" + attval + "']" );
 
-		function appendToVisibleContainer(){
-			if( !$parent.is(':visible') ){
-				$parent = $set.filter(':visible').first();
-				$self.appendTo($parent);
-	      	}
-	    }
-      
-	    appendToVisibleContainer();
-      
-	    $(window).bind( "resize", appendToVisibleContainer );
-      
-	  });
+			function appendToVisibleContainer(){
+				if ( ! $parent.is( ':visible' ) ) {
+					$parent = $set.filter( ':visible' ).first();
+					$self.appendTo( $parent );
+				}
+			}
+
+			appendToVisibleContainer();
+			$(window).bind( "resize", appendToVisibleContainer );
+		});
 	};
 }( jQuery ));
