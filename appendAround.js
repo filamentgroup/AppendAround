e5a2038087806237e0b5aab226271e44f0d33fe5
Rob Miller <rob@bigfish.co.uk>
2013-06-05 16:10:18 +0100

    Rewrite in pure Javascript
    This commit removes any reliance on jQuery, allowing it to be embedded
    in any page.
    
    The call changes from $.appendAround('foo') to appendAround('foo').

--- appendAround.js
+++ appendAround.js
@@ -1,42 +1,40 @@
-/*! appendAround markup pattern. [c]2012, @scottjehl, Filament Group, Inc. MIT/GPL 
-how-to:
-	1. Insert potential element containers throughout the DOM
-	2. give each container a data-set attribute with a value that matches all other containers' values
-	3. Place your appendAround content in one of the potential containers
-	4. Call appendAround() on that element when the DOM is ready
-*/
-(function( $ ){
-	$.fn.appendAround = function(){
-	  return this.each(function(){
-      
-	    var $self = $( this ),
-	        att = "data-set",
-	        $parent = $self.parent(), 
-	        parent = $parent[ 0 ],
-	        attval = $parent.attr( att ),
-	        $set = $( "["+ att +"='" + attval + "']" );
+/*! appendAround markup pattern. [c]2012, @scottjehl, Filament Group, Inc. MIT/GPL
+  how-to:
+  1. Insert potential element containers throughout the DOM
+  2. give each container a data-set attribute with a value that matches all other containers' values
+  3. Place your appendAround content in one of the potential containers
+  4. Call appendAround() on that element when the DOM is ready
+  */
+(function(global) {
+	global.appendAround = function(selector) {
+		var elems, current, parent, att, attval, set;
 
-		function isHidden( elem ){
-			return window.getComputedStyle( elem ,null).getPropertyValue( "display" ) === "none";
+		function isHidden(elem) {
+			return window.getComputedStyle(elem, null).getPropertyValue('display') === 'none';
 		}
 
-		function appendToVisibleContainer(){
-			if( isHidden( parent ) ){
-				var found = 0;
-				$set.each(function(){
-					if( !isHidden( this ) && !found ){
-						$self.appendTo( this );
-						found++;
-						parent = this;
+		function appendToVisibleContainer() {
+			if ( isHidden(parent) ) {
+				for ( var i = 0; i < set.length; i++ ) {
+					if ( !isHidden(set[i]) ) {
+						parent = set[i];
+						parent.appendChild(current);
+						break;
 					}
-				});
-	      	}
-	    }
-      
-	    appendToVisibleContainer();
-      
-	    $(window).bind( "resize", appendToVisibleContainer );
-      
-	  });
+				}
+			}
+		}
+
+		elems = document.querySelectorAll(selector);
+		for ( var i = 0; i < elems.length; i++ ) {
+			current = elems[i];
+			parent = current.parentNode;
+			att = 'data-set';
+			attval = parent.getAttribute(att);
+			set = document.querySelectorAll('[' + att + '=' + attval + ']');
+
+			appendToVisibleContainer();
+			window.addEventListener('resize', appendToVisibleContainer);
+		}
 	};
-}( jQuery ));
+})(window);
